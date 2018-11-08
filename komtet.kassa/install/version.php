d2d51c18cd547751e0e143542c93c2cfecea1a33
Unknown <zeleng_han@mail.ru>
2018-11-08 14:44:04 +0300

    правка стоимости суммы позиций заказа в версиях до 15

--- komtet.kassa/install/version.php
+++ komtet.kassa/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "0.3.1",
-    "VERSION_DATE" => "2018-11-08 14:00:00"
+    "VERSION" => "0.3.2",
+    "VERSION_DATE" => "2018-11-08 15:00:00"
 );
