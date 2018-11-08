6f5da4c8245ab5c482be77274025b44ee2328840
Unknown <zeleng_han@mail.ru>
2018-11-08 14:29:47 +0300

    правка стоимости одной единицы в позиции заказа в версиях до 15

--- komtet.kassa/install/version.php
+++ komtet.kassa/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "0.3.0",
-    "VERSION_DATE" => "2018-11-08 12:00:00"
+    "VERSION" => "0.3.1",
+    "VERSION_DATE" => "2018-11-08 14:00:00"
 );
