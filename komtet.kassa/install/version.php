5ce27306debe5d0ceecf82ebca8d5a8e092cf444
artem <zeleng_han@mail.ru>
2020-11-05 11:33:29 +0300

    v up

--- komtet.kassa/install/version.php
+++ komtet.kassa/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "0.8.1",
-    "VERSION_DATE" => "2020-07-30 15:00:00"
+    "VERSION" => "0.9.0",
+    "VERSION_DATE" => "2020-08-05 15:00:00"
 );
