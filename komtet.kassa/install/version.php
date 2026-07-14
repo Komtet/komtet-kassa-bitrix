9b3b275c7bfef7e7fdbe3533539446011e2e9061
Artem Bychkov <zeleng_han@mail.ru>
2026-07-14 16:47:30 +0300

    version up

--- komtet.kassa/install/version.php
+++ komtet.kassa/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "1.0.0",
-    "VERSION_DATE" => "2026-07-14 16:00:00"
+    "VERSION" => "1.0.1",
+    "VERSION_DATE" => "2026-07-14 17:00:00"
 );
