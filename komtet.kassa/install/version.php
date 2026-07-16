0a3b281f9d1eed4bf44e212243cdf1f1e91411ed
Artem Bychkov <zeleng_han@mail.ru>
2026-07-16 17:51:26 +0300

    фикс sectoral props

--- komtet.kassa/install/version.php
+++ komtet.kassa/install/version.php
@@ -1,6 +1,6 @@
 <?php
 
 $arModuleVersion = array(
-    "VERSION" => "1.0.1",
-    "VERSION_DATE" => "2026-07-14 17:00:00"
+    "VERSION" => "1.0.2",
+    "VERSION_DATE" => "2026-07-15 17:00:00"
 );
